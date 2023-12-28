97baf55f27daa37fc87eb48fffef994222b95513
Raffael Herzog <herzog@raffael.ch>
2023-12-28 09:39:44 +0100

    Fix standalone console not working on modern Java

--- frontend/src/main/groovy/net/netconomy/tools/restflow/frontend/RestFlowConsole.groovy
+++ frontend/src/main/groovy/net/netconomy/tools/restflow/frontend/RestFlowConsole.groovy
@@ -8,6 +8,7 @@ import org.codehaus.groovy.control.CompilationFailedException
 
 import javax.swing.*
 import javax.swing.filechooser.FileNameExtensionFilter
+import java.awt.Component
 import java.nio.file.Path
 import java.util.prefs.Preferences
 
@@ -16,54 +17,70 @@ import java.util.prefs.Preferences
  * @since 2018-10-18
  * @author Raffael Herzog (r.herzog@netconomy.net)
  */
-class RestFlowConsole extends Console {
+class RestFlowConsole {
 
     static List<Path> profilePaths = []
     RestFlow restFlow
+    final Console groovyConsole
 
     RestFlowConsole(Binding binding = new Binding()) {
         this(null, binding)
     }
 
     RestFlowConsole(ClassLoader parent, Binding binding = new Binding()) {
-        super(parent ?: RestFlowConsole.class.classLoader, binding)
-        maxOutputChars = Integer.MAX_VALUE
+        groovyConsole = new Console(parent ?: getClass().getClassLoader(), binding)
+        groovyConsole.maxOutputChars = Integer.MAX_VALUE
+        installConsoleMethods()
+        groovyConsole.newScript(parent, binding)
     }
 
-    @Override
-    void newScript(ClassLoader parent, Binding binding) {
-        restFlow = new RestFlow(new ProfileLoader(parent, profilePaths))
-        config = RestFlowScripts.newCompilerConfiguration(threadInterrupt)
-        shell = new GroovyShell(parent, binding, config) {
-            @Override
-            Object run(String scriptText, String fileName, List list) throws CompilationFailedException {
-                RestFlowScripts.withShell(shell) {
-                    RestFlowScripts.run(RestFlowScripts.parse(shell, restFlow, scriptText, fileName))
+    /**
+     * We run into some access control problems with modern Java when extending
+     * the console. As a workaround, we just use the metaClass to override the
+     * methods we need to override.
+     */
+    private void installConsoleMethods() {
+        groovyConsole.metaClass.newScript = {ClassLoader parent, Binding binding ->
+            final self = (Console)delegate
+            parent = parent ?: this.getClass().getClassLoader()
+            restFlow = new RestFlow(new ProfileLoader(parent, profilePaths))
+            final c = RestFlowScripts.newCompilerConfiguration(self.threadInterrupt)
+            self.config = c
+            self.shell = new GroovyShell(parent, binding, c) {
+
+                @Override
+                Object run(String scriptText, String fileName, List list) throws CompilationFailedException {
+                    RestFlowScripts.withShell(this) {
+                        RestFlowScripts.run(RestFlowScripts.parse(this, restFlow, scriptText, fileName))
+                    }
                 }
+            }
+        }
+        groovyConsole.metaClass.selectFilename = {name = 'Open' ->
+            final self = (Console)delegate
+            final fc = new JFileChooser(self.currentFileChooserDir)
+            fc.fileSelectionMode = JFileChooser.FILES_ONLY
+            newFileNameExtensionFilter('RESTflow Script Files', 'restflow').with {
+                fc.addChoosableFileFilter(it)
+                fc.setFileFilter(it)
+            }
+            fc.addChoosableFileFilter(newFileNameExtensionFilter('Groovy Script Files', 'groovy', 'gvy', 'gy'))
+            fc.acceptAllFileFilterUsed = true
+            if(name == 'Save') {
+                fc.selectedFile = new File('*.restflow')
+            }
+            if (fc.showDialog(self.frame as Component, name as String) == JFileChooser.APPROVE_OPTION) {
+                self.currentFileChooserDir = fc.currentDirectory
+                Preferences.userNodeForPackage(java.io.Console).put('currentFileChooserDir', self.currentFileChooserDir.path)
+                return fc.selectedFile
+            } else {
+                return null
             }
         }
     }
 
-    def selectFilename(name = 'Open') {
-        def fc = new JFileChooser(currentFileChooserDir)
-        fc.fileSelectionMode = JFileChooser.FILES_ONLY
-        newFileNameExtensionFilter('RESTflow Script Files', 'restflow').with {
-            fc.addChoosableFileFilter(it)
-            fc.setFileFilter(it)
-        }
-        fc.addChoosableFileFilter(newFileNameExtensionFilter('Groovy Script Files', 'groovy', 'gvy', 'gy'))
-        fc.acceptAllFileFilterUsed = true
-        if(name == 'Save') {
-            fc.selectedFile = new File('*.restflow')
-        }
-        //noinspection GroovyAssignabilityCheck
-        if (fc.showDialog(frame, name) == JFileChooser.APPROVE_OPTION) {
-            currentFileChooserDir = fc.currentDirectory
-            Preferences.userNodeForPackage(java.io.Console).put('currentFileChooserDir', currentFileChooserDir.path)
-            return fc.selectedFile
-        } else {
-            return null
-        }
+    void run() {
+        groovyConsole.run()
     }
 
     private static newFileNameExtensionFilter(String description, String... extensions) {
@@ -75,10 +92,10 @@ class RestFlowConsole extends Console {
         def cmdLine = new CommandLine().readArgs(args)
         profilePaths.addAll cmdLine.profilePaths
         def console = new RestFlowConsole(RestFlowConsole.getClassLoader())
-        console.useScriptClassLoaderForScriptExecution = true
+        console.groovyConsole.useScriptClassLoaderForScriptExecution = true
         console.run()
         if (cmdLine.files) {
-            console.loadScriptFile(cmdLine.files.first() as File)
+            console.groovyConsole.loadScriptFile(cmdLine.files.first() as File)
         }
     }
 }
